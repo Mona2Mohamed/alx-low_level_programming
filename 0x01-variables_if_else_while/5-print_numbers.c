ad6158b28a2e9175ad97335953b00f601200f5ae
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-04-21 19:49:10 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdil.h>
+
+/**
+*main-program entry point
+*Return:0 no error
+*/
+
+int main(void)
+{
+	char i;
+	for ( i = '0'; i <= '9'; i++ )
+	{
+		putchar(i) ;
+	}
+	putchar('\n') ;
+	return (0) ;
+}
