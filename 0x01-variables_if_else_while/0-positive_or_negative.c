3b226369ae14d5679f09befbf71b7baf1c070bd2
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-04-24 10:54:16 -0700

    0-programmint

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,24 @@
 
 /**
  * *main-program entry point
- * Return:0 if no error ,non-zero value if error.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-
+	{
 		printf("%d is zero\n", n);
-	else 
+	}
+	else
+	{
 		printf("%d is negative\n", n);
-
-return (0);
+	}
+	return (0);
 }
