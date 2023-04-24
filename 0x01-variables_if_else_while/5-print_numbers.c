69d4b0bacc8bd0c5bb2f34666ecbb30b63ba867d
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-04-24 12:40:03 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,14 +4,13 @@
 *main-program entry point
 *Return:0 no error
 */
-
 int main(void)
 {
 	char i;
-	for ( i = '0'; i <= '9'; i++ )
-	{
-		putchar(i) ;
-	}
-	putchar('\n') ;
-	return (0) ;
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
 }
