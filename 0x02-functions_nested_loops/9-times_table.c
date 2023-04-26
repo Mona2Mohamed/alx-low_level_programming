b00afd420020c3314e014790048e7cabc922e93a
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-04-26 16:35:55 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * times_table - Prints the 9 times table
 *
@@ -10,23 +11,23 @@ void times_table(void)
 
 for (a = 0; a <= 9; a++)
 {
-	_putchar(48);
+	_putchar('0');
 
 	for (b = 1; b <= 9; b++)
 	{
+
 		op = a * b;
-		_putchar(44);
-		_putchar(32);
+		_putchar(',');
+		_putchar(' ');
 		if (op <= 9)
 		{
-			_putchar(op + 32);
-			_putchar(op + 48);
+			_putchar(' ');
 		}
 		else
 		{
-			_putchar((op / 10) + 48);
-			_putchar((op % 10) + 48);
+			_putchar((op / 10) + '0');
 		}
+		_putchar((op % 10) + '0');
 	}
 	_putchar('\n');
 }
