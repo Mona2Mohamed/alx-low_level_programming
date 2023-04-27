68ce09574a03acc9a51dcb81b07878fc493fb65a
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-04-26 17:06:44 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,28 +16,22 @@ void print_times_table(int n)
 			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
-				_putchar(44);
-				_putchar(32);
+				_putchar(',');
+				_putchar(' ');
 				if (op <= 9)
+					_putchar(' ');
+				if (op <= 99)
+					_putchar(' ');
+				if (op >= 100)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				else if (op <= 99)
-				{
-					_putchar(32);
+					_putchar((op / 100) + 48);
+					_putchar((op / 10) % 10 + 48);
+				}
+				else if (op <= 99 && op >= 10)
 					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				}
-				_putchar('\n');
-				}
+				_putchar((op % 10) + 48);
 			}
+			_putchar('\n');
 		}
 	}
 }
