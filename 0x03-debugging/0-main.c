c01a8ff217fad670841e75cf3e03f143416dcf58
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-04-27 19:41:31 -0700

    0X03 -C Debbuging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,22 @@
 * main - Test function for positive or negative
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+int n;
+n = 0;
+positive_or_negative(n);
 return (0);
+}
+void positive_or_negative(int n)
+{
+srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 }
