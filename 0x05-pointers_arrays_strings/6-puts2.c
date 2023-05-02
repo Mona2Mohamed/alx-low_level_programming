6a6e85e0019eec7da50c01573ec79213a98cee22
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-05-02 16:35:23 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,10 @@ void puts2(char *str)
 {
 	int l;
 
-	for (l = 0; str[l] != '0'; l++)
+	for (l = 0; str[l] != '\0'; l++)
 	{
 		if (str[l] % 2 == 0)
 			_putchar(str[l]);
 	}
 	_putchar('\n');
-
 }
