00665a86873a3818581729f881c4273e06f4c6d6
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-05-04 17:27:43 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,45 +2,22 @@
 
 
 /**
-
  * print_number - print numbers chars
-
- * @n: integer params
-
- * Return: 0
-
+ * @n:integer params
+ * Return: Always 0
  */
-
-
 void print_number(int n)
-
 {
+	unsigned int n1;
 
-        unsigned int n1;
+	n1 = n;
 
+	if (n < 0)
+	{	_putchar('-');
+		n1 = -n;
+	}
 
-        n1 = n;
-
-
-        if (n < 0)
-
-        {
-
-                _putchar('-');
-
-                n1 = -n;
-
-        }
-
-
-        if (n1 / 10 != 0)
-
-        {
-
-                print_number(n1 / 10);
-
-        }
-
-        _putchar((n1 % 10) + '0');
-
+	if (n1 / 10 != 0)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
