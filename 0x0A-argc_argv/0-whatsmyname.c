db17fdbf96910edbb50729d0a7ede02b8e524dbb
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-05-12 12:04:00 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char const *argv)
+int main(int argc, char const *argv[])
 {
 	void(argc);
 
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
